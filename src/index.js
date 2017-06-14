ee7617fe3cb0e115aab2aa6d7af148a0634419b7
Rovaniemi <mauri.karlin@helsinki.fi>
2017-06-14 14:22:48 +0300

    added fonts

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
-import styles from 'main.css';
-import './assets/css/fonts.css';
+import styles from './main.css';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Montserrat:300,400,700', 'sans-serif']
+  }
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
