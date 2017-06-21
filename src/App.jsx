79f32a7b9272b88789f922f915b3b28ee6b00520
Rovaniemi <mauri.karlin@helsinki.fi>
2017-06-21 16:04:36 +0300

    new data handling

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,26 @@ class App extends Component {
 
     constructor() {
         super();
-        this.state = { post: { algorithm: "", sLat: 60.162059, sLon: 24.945831, gLat: 0, gLon: 0, city: "" }, center: { lat: 60.162059, lng: 24.945831 } }
-
+        this.state = { path: [{ lat: 60.162059, lng: 24.945831 }],center: { lat: 60.162059, lng: 24.945831 }, post: {algorithm: "astar", points:[0,0]}};
         this.handleMenuUpdate = this.handleMenuUpdate.bind(this);
         this.apiCall = this.apiCall.bind(this);
     }
 
     handleMenuUpdate(menuValue) {
-        console.log(this.state.center)
-        var center = { lat: menuValue.sLat, lng: menuValue.sLon }
-        var lastPoint = { lat: menuValue.gLat, lng: menuValue.gLon }
-        var array = []
-        array.push(center)
-        array.push(lastPoint)
-        this.setState({
-            post: menuValue,
-            center: center,
-            path: array
-        });
         this.apiCall(menuValue);
+
+        this.setState({
+            post: menuValue
+        });
+
+        var center = this.state.path[0]
+        this.setState({
+            center: center
+        });
     }
 
     apiCall(post) {
-        fetch('/shortest/0.1V/', {
+        fetch('/0.1v/shortest/', {
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -38,58 +35,35 @@ class App extends Component {
             },
             body: JSON.stringify({
                 algorithm: post.algorithm,
-                sLat: post.sLat,
-                sLon: post.sLon,
-                gLat: post.gLat,
-                gLon: post.gLon,
-                city: post.city
+                points: post.points
             })
-        }).then(response => response.json()).then(json => console.log(this.setState({ path: json.shortestPath })));
+        }).then(response => response.json()).then(json => this.setState({ path: json.shortestPath }));
     }
 
-    apiCallRandom(post) {
-        fetch('/random/0.1V/', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                city: post.city
-            })
-        }).then(response => response.json()).then(json => console.log(this.setState({ path: json.shortestPath })));
-    }
 
     render() {
-        return ( <
-            Row >
-            <
-            Col md = "3"
-            className = "menuCol" >
-            <
-            Menu updateMenu = { this.handleMenuUpdate } > < /Menu> <
-            /Col> <
-            Col md = "9"
-            className = "msCol" >
-            <
-            Row className = "mapRow" >
-            <
-            Col md = "12"
-            className = "msCol" >
-            <
-            Map center = { this.state.center }
-            path = { this.state.path } > < /Map> <
-            /Col> <
-            /Row> <
-            Row className = "statsBoxRow" >
-            <
-            StatsBox > < /StatsBox> <
-            StatsBox > < /StatsBox> <
-            StatsBox > < /StatsBox> <
-            StatsBox > < /StatsBox> <
-            /Row> <
-            /Col> <
-            /Row>
+        return ( 
+        <Row>
+            <Col md = "3" className = "menuCol">
+                <Menu updateMenu = { this.handleMenuUpdate }> 
+                </Menu> 
+            </Col> 
+            <Col md = "9" className = "msCol">
+                <Row className = "mapRow">
+                    <Col md = "12" className = "msCol">
+                        <Map center = {this.state.center}
+                        path = {this.state.path}> 
+                        </Map> 
+                    </Col>   
+                </Row> 
+                <Row className = "statsBoxRow">
+                    <StatsBox></StatsBox> 
+                    <StatsBox></StatsBox> 
+                    <StatsBox></StatsBox> 
+                    <StatsBox></StatsBox> 
+                </Row> 
+            </Col> 
+        </Row>
         );
     }
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -9,55 +9,38 @@ class Menu extends Component {
 
     constructor (props) {
         super(props);
-        this.state = { algoSelected: 1, citySelected: 1 };
+        this.state = { algoSelected: 1, points: []};
         this.algoClick = this.onAlgoClick.bind(this);
-        this.cityClick = this.onCityClick.bind(this);
+        this.handleNewPoints = this.handleNewPoints.bind(this);
+        this.handleNewPoints();
     }
 
     onAlgoClick(algoSelected) {
         this.setState({ algoSelected });
     }
 
-    onCityClick(citySelected) {
-        this.setState({ citySelected });
-    }
-
-    random(){
-
-    }
-
     handleMenuChange() {
-        var post = {algorithm:"", sLat:0, sLon:0, gLat:0, gLon:0, city:""}
-        
+        var post = {algorithm:"", points:[]}
         if(this.state.algoSelected == 1){
             post.algorithm = "astar";
         } else if (this.state.algoSelected == 2){
             post.algorithm = "dijkstra";
-        } else {
-            post.algorithm = "bfs";
         }
-
-        if(this.state.citySelected == 4){
-            post.sLat = Math.random() * (65.8612 - 65.8087) + 65.8087;
-            post.sLon = Math.random() * (24.2152- 24.0916) + 24.0916;
-            post.gLat = Math.random() * (65.8612 - 65.8087) + 65.8087;
-            post.gLon = Math.random() * (24.2152- 24.0916) + 24.0916;
-            post.city = "tornio";
-        }
-
-        if(this.state.citySelected == 1){
-            post.sLat = Math.random() * (60.2957 - 60.1415) + 60.1415;
-            post.sLon = Math.random() * (24.6753- 25.1697) + 25.1697;
-            post.gLat = Math.random() * (60.2957 - 60.1415) + 60.1415;
-            post.gLon = Math.random() * (24.6753- 25.1697) + 25.1697;
-            post.city = "helsinki";
-        }
-
+        post.points = this.state.points;
         this.props.updateMenu(post);
     }
 
-
-
+    handleNewPoints(){
+        fetch('/0.1v/randompoints/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({})
+        }).then(response => response.json()).then(json => this.setState({ points: json}))
+    }
+    
     render() {
         return (
             <div className="menu">
@@ -69,31 +52,17 @@ class Menu extends Component {
                     <p>Visumap is an illustrator for single-source shortest path problem using openstreetmap data in visualisation.</p>
                 </div>
                 <div className="space"></div>
-                <div className="menuCity">
-                    <h6>Choose City</h6>
-                    <ButtonGroup>
-                        <Button color="primary" onClick={() => this.onCityClick(1)} active={this.state.citySelected === 1}>Helsinki</Button>
-                        <Button color="primary" onClick={() => this.onCityClick(2)} active={this.state.citySelected === 2}>Rovaniemi</Button>
-                        <Button color="primary" onClick={() => this.onCityClick(3)} active={this.state.citySelected === 3}>Ivalo</Button>
-                    </ButtonGroup>
-                    <ButtonGroup>
-                        <Button color="primary" onClick={() => this.onCityClick(4)} active={this.state.citySelected === 4}>Tornio</Button>
-                        <Button color="primary" onClick={() => this.onCityClick(5)} active={this.state.citySelected === 5}>Turku</Button>
-                    </ButtonGroup>
-                </div>
-                <div className="space"></div>
                 <div className="menuAlgorithms">
                     <h6>Algorithm</h6>
                     <ButtonGroup>
                         <Button color="primary" onClick={() => this.onAlgoClick(1)} active={this.state.algoSelected === 1}>Astar</Button>
                         <Button color="primary" onClick={() => this.onAlgoClick(2)} active={this.state.algoSelected === 2}>Dijkstra</Button>
-                        <Button color="primary" onClick={() => this.onAlgoClick(3)} active={this.state.algoSelected === 3}>Bfs</Button>
                     </ButtonGroup>
                 </div>
                 <div className="space"></div>
                 <div className="submit">
                     <Button color="primary" size="m" block onClick={() => this.handleMenuChange()}>Vizualise</Button>
-                    <Button color="primary" size="m" block>Get new points</Button>
+                    <Button color="primary" size="m" block onClick={() => this.handleNewPoints()}>Get new points</Button>
                 </div>
 
                 <div className="bottom">
